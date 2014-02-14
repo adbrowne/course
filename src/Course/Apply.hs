272692f8ccc5d2b0524d882da0747430d1c4db64
= <=>
2014-02-14 20:02:18 +1100

    Apply half complete

--- src/Course/Apply.hs
+++ src/Course/Apply.hs
@@ -24,16 +24,17 @@ infixl 4 <*>
 -- >>> Id (+10) <*> Id 8
 -- Id 18
 instance Apply Id where
-  (<*>) =
-    error "todo"
+  (<*>) (Id f) (Id a) = Id(f a)
 
 -- | Implement @Apply@ instance for @List@.
 --
 -- >>> (+1) :. (*2) :. Nil <*> 1 :. 2 :. 3 :. Nil
 -- [2,3,4,2,4,6]
 instance Apply List where
-  (<*>) =
-    error "todo"
+  (<*>) Nil _ = Nil
+  (<*>) (f:.fs) values = 
+    (f <$> values) ++ (fs <*> values)
+
 
 -- | Implement @Apply@ instance for @Optional@.
 --
@@ -46,8 +47,9 @@ instance Apply List where
 -- >>> Full (+8) <*> Empty
 -- Empty
 instance Apply Optional where
-  (<*>) =
-    error "todo"
+  (<*>) Empty _ = Empty
+  (<*>) _ Empty = Empty
+  (<*>) (Full f) (Full v) = Full(f v)
 
 -- | Implement @Apply@ instance for reader.
 --
@@ -66,8 +68,7 @@ instance Apply Optional where
 -- >>> ((*) <*> (+2)) 3
 -- 15
 instance Apply ((->) t) where
-  (<*>) =
-    error "todo"
+  (<*>) f v x = f x (v x)
 
 -- | Apply a binary function in the environment.
 --
@@ -94,8 +95,7 @@ lift2 ::
   -> f a
   -> f b
   -> f c
-lift2 =
-  error "todo"
+lift2 f a b = f <$> a <*> b
 
 -- | Apply a ternary function in the Monad environment.
 --
@@ -126,8 +126,8 @@ lift3 ::
   -> f b
   -> f c
   -> f d
-lift3 =
-  error "todo"
+lift3 f a b c = 
+  f <$> a <*> b  <*> c
 
 -- | Apply a quaternary function in the environment.
 --
@@ -159,9 +159,8 @@ lift4 ::
   -> f c
   -> f d
   -> f e
-lift4 =
-  error "todo"
-
+lift4 f a b c d =
+  f <$> a <*> b <*> c <*> d
 -- | Sequence, discarding the value of the first argument.
 -- Pronounced, right apply.
 --
