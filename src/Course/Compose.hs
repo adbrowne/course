f1f5491fed62fa46397aabe78bd97c4ad52b29cd
Andrew Browne <brownie@brownie.com.au>
2015-01-25 17:55:52 +1100

    Compose complete

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -17,23 +17,37 @@ newtype Compose f g a =
 -- Implement a Functor instance for Compose
 instance (Functor f, Functor g) =>
     Functor (Compose f g) where
-  (<$>) =
-    error "todo"
+  (<$>) f c =
+    let 
+      Compose i = c
+      cb =(\ga -> f <$> ga) <$> i
+    in 
+      Compose cb
 
 instance (Apply f, Apply g) =>
   Apply (Compose f g) where
 -- Implement the (<*>) function for an Apply instance for Compose
-  (<*>) =
-    error "todo"
+  Compose f <*> Compose a =
+      let 
+        bar = (<*>)
+        foo = bar <$> f
+      in
+        Compose (foo <*> a)
 
 instance (Applicative f, Applicative g) =>
   Applicative (Compose f g) where
 -- Implement the pure function for an Applicative instance for Compose
-  pure =
-    error "todo"
+  pure a =
+    Compose $ pure (pure a) 
 
 instance (Bind f, Bind g) =>
   Bind (Compose f g) where
 -- Implement the (=<<) function for a Bind instance for Compose
+-- I don't think this can be done because we have no way to break
+-- down f to build it up gradually
   (=<<) =
     error "todo"
+    {--let 
+      Compose fga = ma
+    in
+      Compose $ f =<< fga --}
